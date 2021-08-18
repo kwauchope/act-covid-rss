3751d72af2c72577de50472bcda7dca9604f7a27
murph <murphc05@gmail.com>
2021-08-18 18:20:14 +1000

    - adding helper functions if we wish to map suburbs to CBR regions

---
+++ helper_fns.py
@@ -0,0 +1,69 @@
+import re
+
+
+CBR_REGIONS = {
+    "Belconnen": [
+        "Aranda", "Belconnen", "Belconnen Town Centre", "Emu Ridge", "Bruce", "Charnwood", "Cook", "Dunlop", "Evatt",
+        "Florey", "Flynn", "Fraser", "Giralang", "Hawker", "Higgins", "Holt", "Kippax Centre", "Kaleen", "Latham",
+        "Lawson", "Macgregor", "Macnamara", "Macquarie", "Jamison Centre", "Jamison", "McKellar", "Melba", "Page",
+        "Scullin", "Spence", "Strathnairn", "Weetangera"
+    ],
+    "Inner North": [
+        "Acton", "Ainslie", "Braddon", "Campbell", "Duntroon", "City", "Canberra City", "Civic", "Dickson",
+        "Dickson Centre", "Downer", "Hackett", "Lyneham", "North Lyneham", "O'Connor", "Reid", "Russell", "Turner",
+        "Watson"
+    ],
+    "Inner South": [
+        "Barton", "Capital Hill", "Deakin", "Forrest", "Fyshwick", "Griffith", "Manuka", "Kingston", "The Causeway",
+        "Narrabundah", "Parkes", "Red Hill", "Yarralumla"
+    ],
+    "Gungahlin": [
+        "Amaroo", "Bonner", "Casey", "Crace", "Forde", "Franklin", "Gungahlin", "Gungahlin Town Centre", "Harrison",
+        "Jacka", "Kenny", "Kinlyside", "Mitchell", "Moncrieff", "Ngunnawal", "Nicholls", "Palmerston", "Taylor",
+        "Throsby"
+    ],
+    "Jerrabomberra": [
+        "Beard", "Hume", "Oaks Estate", "Symonston", "Jerrabomberra"
+    ],
+    "Majura": [
+        "Canberra Airport", "Airport", "Pialligo", "Majura Park", "Majura"
+    ],
+    "Molonglo Valley": [
+        "Denman Prospect", "Coombs", "Molonglo", "Molonglo Valley", "Sulman", "Whitlam", "Wright"
+    ],
+    "Tuggeranong": [
+        "Banks", "Bonython", "Calwell", "Chisholm", "Conder", "Fadden", "Gilmore", "Gordon", "Gowrie", "Greenway",
+        "Tuggeranong Town Centre", "Isabella Plains", "Kambah", "Kambah Village Centre", "Macarthur", "Monash", "Oxley",
+        "Richardson", "Theodore", "Wanniassa", "Erindale Centre", "Tuggeranong"
+    ],
+    "Weston Creek": [
+        "Chapman", "Duffy", "Fisher", "Holder", "Rivett", "Stirling", "Waramanga", "Weston", "Weston Creek",
+        "Weston Creek Centre"
+    ],
+    "Woden Valley": [
+        "Chifley", "Curtin", "Curtin Centre", "Farrer", "Garran", "Hughes", "Isaacs", "Lyons", "Mawson",
+        "Southlands Centre", "O'Malley", "Pearce", "Phillip", "Woden", "Woden Town Centre", "Swinger Hill", "Torrens"
+    ]
+}
+
+
+def normalise_suburb(suburb):
+    """ Function for consistent normalisation before comparison """
+    return re.sub(r'[\W]+', '', suburb.lower())
+
+
+NORMALISED_CBR_REGIONS = {r: list(map(normalise_suburb, CBR_REGIONS[r])) for r in CBR_REGIONS}
+
+
+def suburb_to_region(suburb):
+    """
+    Maps CBR suburbs to CBR regions, suburbs sourced from https://en.wikipedia.org/wiki/List_of_Canberra_suburbs
+    :param suburb: str of suburb
+    :return: str of region
+    """
+
+    normalised_suburb = normalise_suburb(suburb)
+    for region in NORMALISED_CBR_REGIONS:
+        if NORMALISED_CBR_REGIONS[region].__contains__(normalised_suburb):
+            return region
+    return "Other"
